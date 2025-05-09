06a9bfec4cf08e84a747bbfd98d55ab346f2e463
moidgh <moidgh85@gmail.com>
2025-05-09 23:50:49 +0330

    nothing

---
+++ part-1/ClickCounterApp.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClickCounterApp extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
